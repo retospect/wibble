84f17061987aca782150a891d2f83ce42ab5e86e
Reto Stamm <reto@retostamm.com>
2023-02-14 10:28:23 -0800

    Fixed import from module

--- src/wibble/__init__.py
+++ src/wibble/__init__.py
@@ -1,3 +1,5 @@
+from .wibble import Wibble
+
 __version__ = "0.0.2"
 
 
--- src/wibble/wobble.py
+++ src/wibble/wobble.py
@@ -1,5 +1,9 @@
 #! python3
 
+from wibble import Wibble
 
 def warble():
     print("In warble2")
+    other = Wibble()
+    other.main_blarg()
+
